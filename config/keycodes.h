103b929626466e1d077c3c2d24beee2a9f52bafb
DoctorMachete <apuntaqueesmuyfacil@gmail.com>
2023-02-14 11:51:51 +0100

    more layout lines

--- config/keycodes.h
+++ config/keycodes.h
@@ -1,5 +1,43 @@
-#define XXXXXXX &none
-#define _______ &none
+#define ___X___ &none
+
+
+#define ___A___ &kp A
+#define ___B___ &kp B
+#define ___C___ &kp C
+#define ___D___ &kp D
+#define ___E___ &kp E
+#define ___F___ &kp F
+#define ___G___ &kp G
+#define ___H___ &kp H
+#define ___I___ &kp I
+#define ___J___ &kp J
+#define ___K___ &kp K
+#define ___L___ &kp L
+#define ___M___ &kp M
+#define ___N___ &kp N
+#define ___O___ &kp O
+#define ___P___ &kp P
+#define ___Q___ &kp Q
+#define ___R___ &kp R
+#define ___S___ &kp S
+#define ___T___ &kp T
+#define ___U___ &kp U
+#define ___V___ &kp V
+#define ___W___ &kp W
+#define ___X___ &kp X
+#define ___Y___ &kp W
+#define ___Z___ &kp Z
+
+#define __TAB__ &none
+#define _LSHFT_ &none
+#define _LCTRL_ &none
+#define _BSPC__ &none
+#define __RET__ &none
+#define __SPC__ &none
+
+
+
+
 
 #define o_TABL_ &kp LS(LG(LEFT_BRACKET))
 #define o_TABR_ &kp LS(LG(RIGHT_BRACKET))
