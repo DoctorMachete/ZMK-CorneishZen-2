e36dc62ffd95993413bc5a59f5c750ccced8754a
DoctorMachete <apuntaqueesmuyfacil@gmail.com>
2023-02-15 17:23:41 +0100

    first layer

--- config/keycodes.h
+++ config/keycodes.h
@@ -1,41 +1,55 @@
-#define ___X___ &none
 
+#define ___XXX___ &none
+#define _________ &trans
+#define ___TEMP__ &none
 
-#define ___A___ &kp A
-#define ___B___ &kp B
-#define ___C___ &kp C
-#define ___D___ &kp D
-#define ___E___ &kp E
-#define ___F___ &kp F
-#define ___G___ &kp G
-#define ___H___ &kp H
-#define ___I___ &kp I
-#define ___J___ &kp J
-#define ___K___ &kp K
-#define ___L___ &kp L
-#define ___M___ &kp M
-#define ___N___ &kp N
-#define ___O___ &kp O
-#define ___P___ &kp P
-#define ___Q___ &kp Q
-#define ___R___ &kp R
-#define ___S___ &kp S
-#define ___T___ &kp T
-#define ___U___ &kp U
-#define ___V___ &kp V
-#define ___W___ &kp W
-#define ___X___ &kp X
-#define ___Y___ &kp W
-#define ___Z___ &kp Z
+#define ____A____ &kp A
+#define ____B____ &kp B
+#define ____C____ &kp C
+#define ____D____ &kp D
+#define ____E____ &kp E
+#define ____F____ &kp F
+#define ____G____ &kp G
+#define ____H____ &kp H
+#define ____I____ &kp I
+#define ____J____ &kp J
+#define ____K____ &kp K
+#define ____L____ &kp L
+#define ____M____ &kp M
+#define ____N____ &kp N
+#define ____O____ &kp O
+#define ____P____ &kp P
+#define ____Q____ &kp Q
+#define ____R____ &kp R
+#define ____S____ &kp S
+#define ____T____ &kp T
+#define ____U____ &kp U
+#define ____V____ &kp V
+#define ____W____ &kp W
+#define ____X____ &kp X
+#define ____Y____ &kp W
+#define ____Z____ &kp Z
+#define ___SPC___ &kp SPACE
 
-#define __TAB__ &none
-#define _LSHFT_ &none
-#define _LCTRL_ &none
-#define _BSPC__ &none
-#define __RET__ &none
-#define __SPC__ &none
-
-
+#define ___TAB___ &kp TAB
+#define __LSHFT__ &kp LSHIFT
+#define __RSHFT__ &kp RSHIFT
+#define __LCTRL__ &kp LCTRL
+#define __RCTRL__ &kp RCTRL
+#define ___LALT__ &kp LALT
+#define ___RALT__ &kp RALT
+#define __BKSPC__ &kp BACKSPACE
+#define ___DEL___ &kp DELETE
+#define __ENTER__ &kp ENTER
+#define __LWIN___ &kp LEFT_WIN
+#define __RWIN___ &kp RIGHT_WIN
+#define __MENU___ &kp K_CONTEXT_MENU
+#define __REPEAT_ &kp key_repeat
+#define ___ESC___ &kp ESC
+#define __COMMA__ &kp KP_COMMA
+#define ___DOT___ &kp KP_DOT
+#define ___FSLH__ &kp FSLH   //   /
+#define __MINUS__ &kp FSLH   //   -
 
 
 
