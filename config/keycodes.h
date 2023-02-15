c776352dbc9c6a7becc63a49a4faf8f40643d1d4
DoctorMachete <apuntaqueesmuyfacil@gmail.com>
2023-02-15 19:25:55 +0100

    nav layer

--- config/keycodes.h
+++ config/keycodes.h
@@ -106,18 +106,3 @@
 
 
 
-
-
-#define o_TABL_ &kp LS(LG(LEFT_BRACKET))
-#define o_TABR_ &kp LS(LG(RIGHT_BRACKET))
-
-#define MAC_UNDO &kp LG(Z)
-#define MAC_REDO &kp LS(LG(Z))
-#define MAC_COPY &kp LG(C)
-#define MAC_PASTE &kp LG(V)
-#define MAC_CUT &kp LG(X)
-#define MAC_SELECT_ALL &kp LG(A)
-
-#define _ARING_ &kp RA(A)
-#define _OSLSH_ &kp RA(O)
-#define _AELIG_ &kp RA(SINGLE_QUOTE)
