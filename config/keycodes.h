0948f57757550ca56343ef2e5f503d27f1836faa
DoctorMachete <apuntaqueesmuyfacil@gmail.com>
2023-02-19 02:51:07 +0100

    ggggg

--- config/keycodes.h
+++ config/keycodes.h
@@ -142,7 +142,7 @@
 #define __ACC_E__ &ht_accent_e 0 E
 #define __ACC_I__ &ht_accent_i 0 I
 #define __ACC_O__ &ht_accent_o 0 O
-#define __ACC_U__ &kp U //&td_accent_dier_u
+#define __ACC_U__ &td_accent_dier_u
 #define KC_ENE    SEMICOLON
 #define _ACC_ENE_ &kp KC_ENE
 
