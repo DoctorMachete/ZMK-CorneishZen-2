a48a973b218934026e705f62e52ba5a3caa14136
DoctorMachete <apuntaqueesmuyfacil@gmail.com>
2023-02-25 03:16:40 +0100

    test no tapdance

--- config/keycodes.h
+++ config/keycodes.h
@@ -223,7 +223,8 @@
 
 
 // LAYER TAPS
-#define _NAVREP__    &td_nav_rep_npad_togglenpad      // NAV-REP + NPADoREP + TOGGLENPAD
+//#define _NAVREP__    &td_nav_rep_npad_togglenpad      // NAV-REP + NPADoREP + TOGGLENPAD
+#define _NAVREP__    &ht_nav_rep NAV 0      // TEST without the tap dance
 #define _NUMSPC__    &td_num_spc_npad_togglenpad
 #define _NPADKP0_    &ht_custom_tog_tap NPAD KP_NUMBER_0
 #define _SFTFUNC_    &td_shift_func_capslock
