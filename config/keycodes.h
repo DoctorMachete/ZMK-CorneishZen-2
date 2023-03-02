1c5185dd9c47890df8007e84abab2d0b10aa6260
DoctorMachete <apuntaqueesmuyfacil@gmail.com>
2023-03-02 21:47:47 +0100

    LEFT NUMPAD

--- config/keycodes.h
+++ config/keycodes.h
@@ -303,10 +303,22 @@
 
 // MOUSE
 #define __TEST1__    &kp A
-#define _M__LEFT_    &mmv MOVE_HOR(-1600) 
-#define _M__DOWN_    &mmv MOVE_VERT(1600) 
-#define __M__UP__    &mmv MOVE_VERT(-1600) 
-#define _M_RIGHT_    &mmv MOVE_HOR(1600) 
+
+#define MOUSESPEED 1600
+#define __M__UP__    &mmv MOVE_VERT(-MOUSESPEED) 
+#define _M_UP_R__    &mmv MOVE(MOUSESPEED, -MOUSESPEED)
+#define _M_RIGHT_    &mmv MOVE_HOR(MOUSESPEED) 
+#define _M__DN_R_    &mmv MOVE(MOUSESPEED, MOUSESPEED)
+#define _M__DOWN_    &mmv MOVE_VERT(MOUSESPEED) 
+#define _M__DN_L_    &mmv MOVE(-MOUSESPEED, MOUSESPEED)
+#define _M__LEFT_    &mmv MOVE_HOR(-MOUSESPEED)
+#define _M__UP_L_    &mmv MOVE(-MOUSESPEED, MOUSESPEED)
+
+
+
+
+
+
 
 #define _M_LCLCK_    &mkp LCLK 
 #define _M_RCLCK_    &mkp RCLK
